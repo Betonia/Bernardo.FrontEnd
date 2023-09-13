78a433326912807a64d4b63f84e370aca166f4e8
Fairytopia <144158292+Betonia@users.noreply.github.com>
2023-09-13 16:45:41 -0300

    Add files via upload

---
+++ frontEndBe/index.js
@@ -0,0 +1,5 @@
+const meuBotao = document.getElementById("meuBotao");
+function redirecionarParaGoogle() {
+    window.location.hjref = "https>://www.ryan.cat"
+}
+meuBotao.addEventListener("click", redirecionarParaGoogle);
